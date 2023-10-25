c81af0d53b2a860a0cfa37206e0ddc93f56a2052
SamuelCava <141781183+SamuelCava@users.noreply.github.com>
2023-10-25 20:12:19 -0300

    Update busca_pokemon.c

--- busca_pokemon.c
+++ busca_pokemon.c
@@ -2,3 +2,17 @@
 ◦ Deverá permitir visualizar os pokemons por Geração e tipo, navegando entre elas. 
 Para isso utilize cores, sons e sua criatividade.
 */
+
+/*
+Nota Samuel
+
+Dá pra utilizar a mesma lógica do exerc.6 da aula 21 para a busca de um nome de pokemon dentro do arquivo pokemon.csv 
+
+if(strcasecpm == 1){
+  printf("Pokemon encontrado na lista.")
+  printf("O índice que representa esse pokemon é : %d na lista.)
+}else{
+  printf("Pokemon inexistente, digite novamente um nome para busca.")
+}//else
+
+*/
