ca308dc75a13cedbdcc739c8094d8860fd3045e7
SamuelCava <141781183+SamuelCava@users.noreply.github.com>
2023-11-01 17:50:09 -0300

    Update carrega.c

--- carrega.c
+++ carrega.c
@@ -9,29 +9,6 @@ carregado em um vetor de tamanho dinâmico na primeira abertura do programa.
 Abrir o arquivo.csv, reconhecer o seu tamanho, alocar memória e salvar em binário
 */
 
-typedef struct {
-    int numero;
-    // mudar esse 20
-    char nome[20];
-    char tipo1[20];
-    char tipo[20];
-    int total;
-    int ataque;
-    int defesa;
-    int ataque_especial;
-    int defesa_especial;
-    int velocidade;
-    int geracao;
-    int lendario; // na verdade é bool aqui
-    char cor[20];
-    float altura;
-    float peso;
-    int taxa_captura;
-    int proxima_evolucao;
-    int pre_evolucao;
-
-}Pokemon;
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
