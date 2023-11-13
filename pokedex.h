a5492070ed622806e7447e558b3f365229fdf712
leodcardoso <146145077+leodcardoso@users.noreply.github.com>
2023-11-12 21:28:23 -0300

    Update pokedex.h

--- pokedex.h
+++ pokedex.h
@@ -3,15 +3,15 @@
 typedef struct{
     int qtd_cadastrados;
     // tamanho representa o espaço de quantos int tenho pra usar
-    int tamanho = 10;
+    int tamanho;
     Pokemon *pokemons;
     
 
-}Pokedex;
+} Pokedex;
 
-void inicia_vetor(Pokedex pokedex);
-void inserir_na_pokedex(Pokemon pokemon, Pokedex pokedex);
-void listar_na_pokedex(Pokedex pokedex);
-void pesquisar_na_pokedex(int codigo, Pokedex pokedex);
-void alterar_na_pokedex(int codigo, Pokedex pokedex);
-void excluir_na_pokedex(int codigo, Pokedex pokedex);
+void inicia_pokedex(Pokedex* pokedex);
+void inserir_na_pokedex(Pokemon* pokemon, Pokedex* pokedex);
+void listar_na_pokedex(Pokedex* pokedex);
+void pesquisar_na_pokedex(int codigo, Pokedex* pokedex);
+void alterar_na_pokedex(int codigo, Pokedex* pokedex);
+void excluir_na_pokedex(int codigo, Pokedex* pokedex);
