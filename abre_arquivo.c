b8424078962d21f8366c67bd889916e2bbccf58a
leodcardoso <146145077+leodcardoso@users.noreply.github.com>
2023-11-16 12:04:29 -0300

    Add files via upload

---
+++ abre_arquivo.c
@@ -0,0 +1,110 @@
+/**
+ * @file abre_arquivo.c
+ * @author Leonardo Dal Poz Cardoso (lcardoso.2005@alunos.utfpr.edu.br) e Samuel Assunção Cavalherie (samuelcavalherie@alunos.utfpr.edu.br)
+ * @brief Arquivo responsável por ler as informações registradas na coleção, mochila e pokedex durante o jogo
+ * @version 0.1
+ * @date 2023-11-14
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "pokedex.h"
+#include "mochila.h"
+#include "pokemon.h"
+#include "colecao.h"
+
+/**
+ * @brief Função responsável por ler as informações registradas no arquivo mochila
+ * 
+ * @param mochila Passa como argumento da função, um ponteiro para tipo struct Mochila
+ */
+
+void abre_mochila(Mochila* mochila){
+    // inicializar mochila com qtd = 0 se n tiver o mochila.dat
+    mochila->pokemons_mochila = 0;
+    FILE *arq;
+    arq = fopen("mochila.dat", "rb");
+    if (arq == NULL){
+        perror("Erro para abrir o arquivo.");      
+        exit(1);   
+    }// if
+    int quantidade_mochila;
+    fread(&quantidade_mochila, sizeof(int), 1, arq);
+    mochila->pokemons_mochila = quantidade_mochila;
+    for (int i = 0; i < quantidade_mochila; i++){
+        int codigo;
+        fread(&codigo, sizeof(int), 1, arq);
+        mochila->codigos_pokemon[i] = codigo;
+    }
+    
+}
+
+/**
+ * @brief Função responsável por ler as informações registradas no arquivo pokedex
+ * 
+ * @param pokedex Passa como argumento da função, um ponteiro para tipo struct Pokedex
+ */
+
+void abre_pokedex(Pokedex* pokedex){
+    inicia_pokedex(pokedex);
+    FILE *arq;
+    arq = fopen("pokedex.dat", "rb");
+     if (arq == NULL){
+        perror("Erro para abrir o arquivo.");      
+        exit(1);   
+    }// if
+    // sei que a primeira coisa sempre é um número então vou verificando se 
+    // numero ,nome        ,tipo1    ,tipo2    ,total ,hp  ,ataque ,defesa ,ataque_especial ,
+    // defesa_especial ,velocidade ,geracao ,lendario ,cor     ,altura_m ,peso_kg ,taxa_captura
+    int numero;
+    Pokemon pokemon;
+    while (fread(&numero, sizeof(int), 1, arq) == 1){
+        pokemon.numero = numero;
+        fread(pokemon.nome, sizeof(char), 30, arq);
+        fread(pokemon.tipo1, sizeof(char), 30, arq);
+        fread(pokemon.tipo2, sizeof(char), 30, arq);
+        fread(&pokemon.total, sizeof(int), 1, arq);
+        fread(&pokemon.hp, sizeof(int), 1, arq);
+        fread(&pokemon.ataque, sizeof(int), 1, arq);
+        fread(&pokemon.defesa, sizeof(int), 1, arq);
+        fread(&pokemon.ataque_especial, sizeof(int), 1, arq);
+        fread(&pokemon.defesa_especial, sizeof(int), 1, arq);
+        fread(&pokemon.velocidade, sizeof(int), 1, arq);
+        fread(&pokemon.geracao, sizeof(int), 1, arq);
+        fread(&pokemon.lendario, sizeof(int), 1, arq);
+        fread(pokemon.cor, sizeof(char), 30, arq);
+        fread(&pokemon.altura, sizeof(double), 1, arq);
+        fread(&pokemon.peso, sizeof(double), 1, arq);
+        fread(&pokemon.taxa_captura, sizeof(int), 1, arq);
+        fread(&pokemon.pre_evolucao, sizeof(int), 1, arq);
+        fread(&pokemon.proxima_evolucao, sizeof(int), 1, arq);
+        inserir_na_pokedex(&pokemon, pokedex);
+    }
+}
+
+/**
+ * @brief Função responsável por ler as informações registradas no arquivo coleção
+ * 
+ * @param colecao Passa como argumento da função, um ponteiro para tipo struct Colecao
+ */
+
+void abre_colecao(Colecao* colecao){
+    inicia_colecao(colecao);
+    FILE *arq;
+    arq = fopen("colecao.dat", "rb");
+    if (arq == NULL){
+        perror("Erro para abrir o arquivo.");      
+        exit(1);   
+    }// if
+    int qtd_colecao;
+    fread(&qtd_colecao, sizeof(int), 1, arq);
+    for (int i = 0; i < qtd_colecao; i++){
+        int codigo;
+        fread(&codigo, sizeof(int), 1, arq);
+        inserir_colecao(codigo, colecao);
+    }
+}
--- menu.c
+++ menu.c
@@ -1,19 +1,180 @@
+/**
+ * @file menu.c
+ * @author  Leonardo Dal Poz Cardoso (lcardoso.2005@alunos.utfpr.edu.br) e Samuel Assunção Cavalherie (samuelcavalherie@alunos.utfpr.edu.br)
+ * @brief Arquivo responsavel por armazenar as funções de menu do jogo
+ * @version 0.1
+ * @date 2023-11-14
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <menu.h>
+#include "menu.h"
+#include "mochila.h"
+#include "colecao.h"
+#include "pokedex.h"
+#include "pokemon.h"
 
-int menu_principal(){
-    printf("[1] exibir pokedex");
-    printf("[2] exibir mochila");
-    printf("[3] exibir itens");
-    printf("[4] batalha pokemon");
-    int opcao;
-    scanf("%d", &opcao);
+/**
+ * @brief Função responsável por restringir a entrada de opção digitada pelo usuário, dentro do intervalo de opções disponíveis
+ * 
+ * @param a 
+ * @param b 
+ * @return int 
+ */
+int intervalo(int a, int b){
+    // fica pedindo o inteiro até que a <= x <= b;
+    int x;
+    scanf("%d", &x);
+    while (!(a <= x && x <= b)){
+        scanf("%d", &x);
+    }
+    return x;
+}
+
+/**
+ * @brief Função responsável por exibir menu de opções da pokedex
+ * 
+ * @param pokedex Passa como argumento da função, um ponteiro para tipo struct Pokedex
+ */
+// verificado
+void menu_pokedex(Pokedex* pokedex){
+    // (inserir/listar/pesquisar/alterar/exclui
+    int qtd_opcoes = 5;
+    printf("[1] inserir\n");
+    printf("[2] listar\n");
+    printf("[3] pesquisar\n");
+    printf("[4] alterar\n");
+    printf("[5] excluir\n");
+    int opcao = intervalo(1, qtd_opcoes);
+    int codigo_pokemon;
+    
+
+    switch (opcao){
+    case 1:
+    // preciso pegar o pokemon aqui dentro
+        Pokemon pokemon;
+        pede_pokemon(&pokemon);
+        inserir_na_pokedex(&pokemon, pokedex);
+        break;
+    case 2:
+        listar_na_pokedex(pokedex);
+        break;
+    case 3:
+        printf("Digite cógido do pokemon: ");
+        scanf("%d", &codigo_pokemon);
+        pesquisar_na_pokedex(codigo_pokemon, pokedex);
+        break;
+    case 4:
+        printf("Digite cógido do pokemon: ");
+        scanf("%d", &codigo_pokemon);
+        alterar_na_pokedex(codigo_pokemon, pokedex);
+        break;
+    case 5:
+        printf("Digite cógido do pokemon: ");
+        scanf("%d", &codigo_pokemon);
+        excluir_na_pokedex(codigo_pokemon, pokedex);
+        break;
+    default:
+        printf("Opção invalida\n");
+        //perror("Opção inválida\n");
+        break;
+    }//switch
+
+    return;
+}
+
+/**
+ * @brief Função responsável por exibir menu de opções da coleção
+ * 
+ * @param colecao Passa como argumento da função, um ponteiro para tipo struct Colecao
+ * @param pokedex Passa como argumento da função, um ponteiro para tipo struct Pokedex
+ */
+// verificado
+void menu_colecao(Colecao* colecao, Pokedex* pokedex){
+    int qtd_opcoes = 5;
+    // remvoer inserir repetido
+    printf("[1] inserir\n");
+    printf("[2] listar\n");
+    printf("[3] pesquisar\n");
+    printf("[4] alterar\n");
+    printf("[5] excluir\n");
+    int opcao = intervalo(1, qtd_opcoes);
+    int codigo_pokemon;
+
+    switch (opcao){
+    case 1:
+        printf("Digite cógido do pokemon: ");
+        scanf("%d", &codigo_pokemon);
+        inserir_colecao(codigo_pokemon, colecao);
+        break;
+    case 2:
+        listar_colecao(colecao, pokedex);
+        break;
+    case 3:
+        printf("Digite cógido do pokemon: ");
+        scanf("%d", &codigo_pokemon);
+        pesquisar_colecao(codigo_pokemon, colecao);
+        break;
+    case 4:
+        printf("Digite cógido do pokemon: ");
+        scanf("%d", &codigo_pokemon);
+        alterar_colecao(codigo_pokemon, colecao);
+        break;
+    case 5:
+        printf("Digite cógido do pokemon: ");
+        scanf("%d", &codigo_pokemon);
+        excluir_colecao(codigo_pokemon, colecao);
+        break;
+    default:
+        perror("Opção inválida\n");
+        break;
+    }//switch
+
+    return;
+}
+
+/**
+ * @brief Função responsável por exibir menu de opções da mochila
+ * 
+ * @param mochila Passa como argumento da função, um ponteiro para tipo struct Mochila
+ * @return int 
+ */
+int menu_mochila(Mochila* mochila, Pokedex* pokedex){
+    int qtd_opcoes = 2;
+    printf("[1] mudar pokemon\n");
+    printf("[2] ver pokemon\n");
+    int opcao = intervalo(1, qtd_opcoes);
+    switch (opcao){
+    case 2:
+        printf("digite a posicao que deseja visualizar: ");
+        int posicao;
+        scanf("%d", &posicao);
+        visualizar_mochila(posicao, mochila, pokedex);
+        break;
+    
+    default:
+        break;
+    }
     return opcao;
 }
 
-
-
-int main(){
-    return 0;
+/**
+ * @brief Função responsável por exibir menu de opções inicial
+ * 
+ * @return int Valor de retorno para indicar a opção escolhida
+ */
+int menu_principal(){
+    int qtd_opcoes = 7;
+    printf("[1] pokedex\n");
+    printf("[2] colecao\n");
+    printf("[3] mochila\n");
+    printf("[4] exibir itens\n");
+    printf("[5] batalha pokemon\n");
+    printf("[6] salvar progresso\n");
+    printf("[7] sair\n");
+    int opcao = intervalo(1, qtd_opcoes);
+    return opcao;
 }
